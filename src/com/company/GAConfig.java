ca45973b7c6c6495f4adcc22125e7de590396a19
bqhuy <bqhuy54@gmail.com>
2022-05-31 08:04:43 +1000

    Experiment 31/05/22

--- src/com/company/GAConfig.java
+++ src/com/company/GAConfig.java
@@ -4,9 +4,10 @@ public class GAConfig {
     public static int populationSize = 100;
     public static int numberOfAgents = 4;
     public static int convergenceCondition = 500;
-//    public static double parameter = 0.5;
     public static int generationLimit = 2000;
     public static int probabilityCrossover = 9;     // 0.9
     public static int probabilityMutation = 2;     // 0.2
+
+    //    public static double parameter = 0.5;
 
 }
